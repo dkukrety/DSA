199a67112feb30d29d02530ae871f085ec264906
Deepak Kukrety <dkukrety@gmail.com>
2023-06-05 23:34:14 +0530

    added bubble sort and corrected selection sort

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -27,10 +27,57 @@ void selection_sort(vector<int> &arr)
     }
 }
 
+// Overall logic is in first pass arr[0] should contain minimum
+// element of array. In second pass arr[1] should have second minimum
+
+// Correct logic is swap only the element present in j range
+// which is less than i. If there is no element less than ith index
+// no need of swap. Unnecessary swaping is reduced here.
+
+void selection_sort_correct_one(vector<int> &arr)
+{
+    // Write your code here.
+    int len = arr.size();
+    int minindex = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        minindex = i;
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[j] < arr[minindex])
+            {
+                minindex = j;
+            }
+        }
+        if (minindex != i)
+            std::swap(arr[i], arr[minindex]);
+    }
+}
+
+// Logic is to bubble down minimum element to arr[0]
+// so keep on comparing with nighbour element.
+// scan of arr starts from end of array.
+
+void bubble_sort(vector<int> &arr)
+{
+    // Write your code here.
+    int len = arr.size();
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = len - 1; j > i; j--)
+        {
+            if (arr[j - 1] > arr[j])
+            {
+                swap(arr[j - 1], arr[j]);
+            }
+        }
+    }
+}
+
 int main()
 {
-    vector<int> arr = {1, 4, 2, 3, 8, 7};
-    selection_sort(arr);
+    vector<int> arr = {8, 7, 6, 5, 4, 3};
+    bubble_sort(arr);
     for (int i = 0; i < arr.size(); i++)
     {
         cout << arr[i] << " ";
