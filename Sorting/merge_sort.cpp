27478bb6085fd6ed1a0b9a4e31ae3d392e5a4a0d
Deepak Kukrety <dkukrety@gmail.com>
2023-06-08 15:27:12 +0530

    some update

---
+++ Sorting/merge_sort.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// merge sort best,worse,average runtime o(nlogn)
+// if add a logic to check if array is already sorted
+// then for any sorting algorithm the runtime will be o(n)
+
+// can we reduce worse case time for any sorting algorithm ?
+
+// for an already sorted array insertion sort will be faster than
+// merge sort
+
+// what is TimSort?
+// Merge sort also need extra space which is function of input, so
+// to optimize merge sort, a hybrid algorithm is used where
+// for small input size insertion sort is used(also the best case of
+// insertion sort is o(n)) and for large input
+// merge sort is used.
+// This algorithm is standard sorting algo in python and in java it is used
+// to sort objects
+
+bool check_if_already_sort(const vector<int> arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (arr[i + 1] < arr[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// vector<int> arr = {5, 6, 3, 2, 1, 9, 7, 6};
+// mergSortHelper(arr,0,8)
+// mid = 0+8/2;
+// mergSortHelper(arr,0,4) and mergSortHelper(arr,4,8)
+
+void mergeSortHelper(vector<int> &arr, int start, int end)
+{
+    if (start >= end - 1)
+        return;
+
+    int mid = (end + start) / 2;
+    mergeSortHelper(arr, start, mid);
+    mergeSortHelper(arr, mid, end);
+
+    vector<int> mList(end - start + 1);
+    int i = start;
+    int k = 0;   // Index for merged List
+    int j = mid; // Start index of 2nd list
+    while (i < mid && j < end)
+    {
+        if (arr[i] <= arr[j])
+        {
+            mList[k++] = arr[i++];
+        }
+        else
+        {
+            mList[k++] = arr[j++];
+        }
+    }
+
+    while (i < mid)
+    {
+        mList[k++] = arr[i++];
+    }
+    while (j < end)
+    {
+        mList[k++] = arr[j++];
+    }
+
+    for (int l = 0; l < k; l++)
+    {
+        arr[start + l] = mList[l];
+    }
+}
+void mergeSort(vector<int> &arr)
+{
+    // idea of whole code below is make best case complexity as o(n)
+    if (arr.size() < 2)
+    {
+        return; // An empty or single-element vector is considered sorted.
+    }
+    if (check_if_already_sort(arr))
+        return;
+
+    mergeSortHelper(arr, 0, arr.size());
+}
+int main()
+{
+    vector<int> arr = {5, 6, 3, 2, 1, 9, 7, 6};
+    mergeSort(arr);
+
+    for (auto &i : arr)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
-// Logic is to select the minimum element from the array
-// and move it to first position. Then choose 2nd min from
-// array and move it to 2nd position and so on.
+// Summary:
+// Selection and Bubble sort sort the array as they go along
+// their best,average and worse case are o(n2)
+// Pattern#1 is brute force
+
+// Insertion sort assumes that there is one sorted array
+// and it has to insert a new element in that sorted array
+// best case is o(n) rest is o(n2)
+/// Pattern#2 is decrease the problem space step by step and then conquer.
+
+// #########################################################
+//  Logic is to select the minimum element from the array
+//  and move it to first position. Then choose 2nd min from
+//  array and move it to 2nd position and so on.
 
 void selection_sort(vector<int> &arr)
 {
@@ -29,10 +40,15 @@ void selection_sort(vector<int> &arr)
 
 // Overall logic is in first pass arr[0] should contain minimum
 // element of array. In second pass arr[1] should have second minimum
+// So scan array from begining to end, find smallest element
+// exchange that element with a[0]. Then scan again from 1 to n-1
+// find next smallest element and exchange with a[1]
 
 // Correct logic is swap only the element present in j range
 // which is less than i. If there is no element less than ith index
 // no need of swap. Unnecessary swaping is reduced here.
+
+// Decrease and Conquer design strategy....
 
 void selection_sort_correct_one(vector<int> &arr)
 {
@@ -56,7 +72,13 @@ void selection_sort_correct_one(vector<int> &arr)
 
 // Logic is to bubble down minimum element to arr[0]
 // so keep on comparing with nighbour element.
-// scan of arr starts from end of array.
+// scan of arr starts from end of array and it keeps
+// on bubbling up minimum element at the begining or
+// array
+
+// Decrease the problem space one by one and Conquer it --> design strategy....
+// basically solve for a[0], and delegate a[n-1]
+// to someone else
 
 void bubble_sort(vector<int> &arr)
 {
@@ -74,10 +96,73 @@ void bubble_sort(vector<int> &arr)
     }
 }
 
+// Here we assume that n-1 array is already sorted. So we need
+// to insert nth element in that n-1 sorted array.
+// So we start comparing nth element from n-1 to 0, shifting
+// element along with it and at end exchange the element....
+
+// So assumption is left end of array is always sorted
+// and we are inserting a new element in sorted array.
+// new element to be inserted is stored in temp.
+// Pattern is decrease the problem space step by step and then conquer.
+
+void insertion_sort(vector<int> &arr)
+{
+    // Write your code here.
+    int len = arr.size();
+    int temp = 0;
+    int redIndex = 0;
+    for (int i = 0; i < len; i++)
+    {
+        temp = arr[i];
+        // redIndex points to already sorted array
+        // this array is at left of not sorted part of
+        // array
+        redIndex = i - 1;
+        while (redIndex >= 0 && arr[redIndex] > temp)
+        {
+            arr[redIndex + 1] = arr[redIndex];
+            redIndex--;
+        }
+        // Position to insert is found in sorted array
+        arr[redIndex + 1] = temp;
+    }
+}
+
+/**
+ * @brief Performs insertion sort on the given vector.
+ * ChatGpt is used to add comments and make this
+ * Doxygen-compatible
+ * @param arr The vector to be sorted.
+ */
+void insertion_sort_chatgpt(std::vector<int> &arr)
+{
+    int len = arr.size();
+    int temp = 0;
+    int redIndex = 0;
+
+    // Iterate through the unsorted portion of the array
+    for (int i = 0; i < len; i++)
+    {
+        temp = arr[i];    // Current element to be inserted at the appropriate position
+        redIndex = i - 1; // Index of the last element in the sorted portion
+
+        // Shift elements to the right until the correct position for the current element is found
+        while (redIndex >= 0 && arr[redIndex] > temp)
+        {
+            arr[redIndex + 1] = arr[redIndex]; // Shift elements to the right
+            redIndex--;
+        }
+
+        // Insert the current element at the correct position in the sorted portion
+        arr[redIndex + 1] = temp;
+    }
+}
+
 int main()
 {
     vector<int> arr = {8, 7, 6, 5, 4, 3};
-    bubble_sort(arr);
+    insertion_sort(arr);
     for (int i = 0; i < arr.size(); i++)
     {
         cout << arr[i] << " ";
